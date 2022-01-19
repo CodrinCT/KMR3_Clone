1d9e89d34b409c1635885177729e749e9ed9d06e
Codrin Tapusa <codrin.tapusa@quartzmatrix.ro>
2022-01-19 16:17:49 +0200

    first

---
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.admin_master')
+ 
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  @include('admin.dashboard.main_content')
+  <!-- /.content-wrapper -->
+@endsection
